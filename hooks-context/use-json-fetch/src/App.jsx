2db1a331943b4769997121cb22145c75d6961562
usmankamayl <usman.kamailaev@yandex.ru>
2023-03-20 19:39:25 +0300

    hooks-context

--- hooks-context/use-json-fetch/src/App.jsx
+++ hooks-context/use-json-fetch/src/App.jsx
@@ -1,20 +1,42 @@
 import './App.css';
 
-import useFetch from "./UseJsonFetch";
+import {useFetch} from "./UseJsonFetch";
+import {useEffect, useState} from "react";
+
 
 function App() {
-  const url = 'http://localhost:7070/loading';
-  const { loading, data, error } = useFetch(url, null);
-  console.log(data, loading, error)
-  return (
-    <div className="App">
-      {loading && <p>load...</p>}
-      <div>
-        {error && <span style={{ color: 'red'}}>{JSON.stringify(error)}</span>}
-      </div>
-      {JSON.stringify(data)}
-    </div>
-  );
-}
 
+  let [url, setUrl] = useState({url: 'http://localhost:7070/data'});
+  const { data, error, loading } =  useFetch(url.url, null);
+  console.log(data, error, loading, url.url,' state');
+  const handleChangeUrl = (newUrl) => {
+   setUrl({...url, url: newUrl})
+  };
+  useEffect(() => {
+    handleChangeUrl(url);
+  }, []);
+
+  if(loading) {
+    return (<p>Loading...</p>)
+  }
+  if (error) {
+    return(<>
+        <p style={{color: "red", textAlign: "center"}}>{JSON.stringify(error)}</p>
+      <button onClick={() => handleChangeUrl('http://localhost:7070/data')}>To data</button>
+          </>
+    )
+  }
+  // const renderItem = ({id, title})=> (
+  //   <div key = {`item-${id}`}>
+  //     <p>{id} - {title}</p>
+  //   </div>
+  // );
+  return(
+    <div className='wrapper'>
+      <p>{JSON.stringify(data)}</p>
+      <button onClick={() => handleChangeUrl('http://localhost:7070/error')}>To error</button>
+      <button onClick={() => handleChangeUrl('http://localhost:7070/loading')}>To loading</button>
+    </div>
+  )
+}
 export default App;
--- hooks-context/use-json-fetch/src/UseJsonFetch.jsx
+++ hooks-context/use-json-fetch/src/UseJsonFetch.jsx
@@ -1,35 +1,69 @@
 
-import {useState, useEffect} from "react";
-
-
-
-const useFetch = (url, options) => {
-
-  const [status, setStatus] = useState({
-    loading: false,
-    data: undefined,
-    error: undefined
-  });
-
-  function fetchNow(url, options) {
-    setStatus({ loading: true });
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((res) => {
-        setStatus({ loading: false, data: res });
-      })
-      .catch((error) => {
-        setStatus({ loading: false, error: error });
-      });
-  }
-
+// import {useState, useEffect} from "react";
+//
+//
+//
+// const useFetch = (url, options) => {
+//
+//   const [state, setState] = useState({
+//     loading: false,
+//     data: undefined,
+//     error: undefined
+//   });
+//
+//   function fetchNow(url, options) {
+//     fetch(url, options)
+//       .then((res) => {
+//         console.log(res.status);
+//         console.log(res, ' json')
+//         if (res.status === 500) {
+//           console.log(res.statusText)
+//           return setState({...state, error: res.statusText});
+//         }
+//       })
+//       .catch((error) => {
+//         console.log('error')
+//         setState({...state,  loading: true, error: error, data: undefined });
+//       });
+//   }
+//
+//   useEffect(() => {
+//     if (url) {
+//       fetchNow(url, options);
+//     }
+//   }, []);
+//   console.log(state, ' state')
+//   return state;
+// }
+//
+// export default useFetch;
+import { useState, useEffect } from 'react';
+export const useFetch = (url, options) => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState( null);
   useEffect(() => {
-    if (url) {
-      fetchNow(url, options);
+    const fetchData = async() => {
+      try {
+        setLoading(true);
+        const response = await fetch(url, options);
+        const json = await response.json();
+        if (response.status === 200) {
+          setLoading(false);
+          setError(null);
+          setData(json);
+        }
+
+        if (response.status === 500) {
+          setError(json);
+          setData(null);
+          setLoading(false);
+        }
+      } catch (err) {
+        console.log(err, ' err')
+      }
     }
-  }, []);
-
-  return {...status, fetchNow};
-}
-
-export default useFetch;
+    fetchData();
+  }, [url, options]);
+  return ({loading, data, error});
+};
