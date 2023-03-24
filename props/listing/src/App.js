1f7e5cc3e6506986b62ed5372e732d63fafc42ee
usmankamayl <usman.kamailaev@yandex.ru>
2023-03-24 16:43:36 +0300

    props

--- props/listing/src/App.js
+++ props/listing/src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import Listing from "./components";
 import data from "./data/etsy.json";
-
-// console.log(data, ' data')
 const db = JSON.parse(JSON.stringify(data));
 
-const list = db.map(item => {
-  return {
-    id: item.listing_id ? item.listing_id : 1111,
-    url: item.url ? item.url : '',
-    img: item.MainImage ? item.MainImage.url_570xN : '',
-    title: item.title ? (item.title.length > 50) ? item.title.slice(0, 50) + '...' :  item.title : 'Title',
-    currency_code: item.currency_code ? item.currency_code : 'GBR',
-    price: item.price ? item.price : 0,
-    quantity: item.quantity ? item.quantity : 0,
-  }
-})
+// const list = db.map(item => {
+//   return {
+//     id: item.listing_id ? item.listing_id : 1111,
+//     url: item.url ? item.url : '',
+//     img: item.MainImage ? item.MainImage.url_570xN : '',
+//     title: item.title ? (item.title.length > 50) ? item.title.slice(0, 50) + '...' :  item.title : 'Title',
+//     currency_code: item.currency_code ? item.currency_code : 'GBR',
+//     price: item.price ? item.price : 0,
+//     quantity: item.quantity ? item.quantity : 0,
+//   }
+// })
+
+const list = db.filter(item => item.state === 'active');
 console.log(list, ' list')
 
 
--- props/listing/src/components/Listing.jsx
+++ props/listing/src/components/Listing.jsx
@@ -3,14 +3,14 @@ import PropTypes, {object} from 'prop-types';
 export const Listing = ({data = []}) => {
   return (
     data.map(item => (
-      <div className="item" key={item.id}>
+      <div className="item" key={item.listing_id}>
         <div className="item-image">
           <a href={item.url}>
-                <img src={item.img} alt="картинка"/>
+                <img src={item.MainImage.url_570xN} alt="картинка"/>
           </a>
         </div>
         <div className="item-details">
-          <p className="item-title">{item.title}</p>
+          <p className="item-title">{item.title.length > 50 ? item.title.slice(0, 50) + '...' :  item.title}</p>
           <p className="item-price">{item.currency_code === 'USD' ? '$' : item.currency_code === 'EUR' ? '€' : 'GBP'} {item.price}</p>
           <p className={item.quantity <= 10  ? "level-low item-quantity" : item.quantity <= 20 ? "level-medium item-quantity" : "level-high item-quantity"} >{item.quantity} left</p>
         </div>
